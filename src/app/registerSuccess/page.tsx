67d68f6fe77c533ed78119b452197fea6081c0e6
Roger <rxu@hoopp.com>
2025-06-14 10:26:36 -0400

    Add route protection for payment and registerSuccess page

--- src/app/registerSuccess/page.tsx
+++ src/app/registerSuccess/page.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+"use client";
+import React, {useEffect} from "react";
 import Link from "next/link";
+import {useRouter} from "next/navigation"
 
 export default function RegisterSuccess() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login"); // redirect to login if no token
+    }
+  }, [router]);
     return (
-        <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-            <h1>Registration Successful!</h1>
-            <p>Your account has been created successfully.</p>
+        <div className="min-h-screen flex flex-col justify-center items-center">
+            <h1 className="text-3xl font-bold mb-4">Registration Successful!</h1>
+            <p className="mb-6">Your account has been created successfully.</p>
             <Link
-                href="/login"
-                style={{ marginTop: 20, color: "#0070f3", textDecoration: "underline" }}
+            href="/login"
+            className="mt-2 text-pink-500 underline hover:text-pink-600"
             >
-                Go to Login
+            Go to Login
             </Link>
         </div>
     );
